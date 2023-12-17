28e377f375e847b8d7f233c611e6733e9e9c2b91
Carlos Vieira <carlosportalsocial@gmail.com>
2023-12-17 15:34:09 -0300

    added routes auth by jwt

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BrandController;
-use App\Models\Customer;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,8 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::prefix('v1')->middleware('jwt.auth')->group(function () {
-    Route::apiResource('cliente', Customer::class);
+Route::prefix('v1')->middleware('jwt.auth')->group(function () {
+    Route::apiResource('cliente', CustomerController::class);
     //Route::apiResource('carro', CarroController::class);
     //Route::apiResource('locacao', LocacaoController::class);
     Route::apiResource('marca', BrandController::class);
@@ -31,6 +31,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
-//});
+});
 
 Route::post('login', [AuthController::class, 'login']);

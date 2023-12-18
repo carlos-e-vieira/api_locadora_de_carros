f3c2b29690e2b56a52259b81eb548f76a1c25e45
Carlos Vieira <carlosportalsocial@gmail.com>
2023-12-18 20:54:12 -0300

    refatored brand exception

--- app/Exceptions/BrandExceptions.php
+++ app/Exceptions/BrandExceptions.php
@@ -9,47 +9,34 @@ use Illuminate\Http\Response;
 
 class BrandExceptions extends Exception
 {
-    private string $error;
-
-    private int $statusCode;
+    private array $errorMessages = [
+        'getAllBrandsPaginated' => [
+            'message' => 'Erro ao listar os dados das marcas',
+            'status' => Response::HTTP_NOT_FOUND,
+        ],
+        'saveBrand' => [
+            'message' => 'Erro ao cadastrar os dados da marca',
+            'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+        ],
+        'getBrandById' => [
+            'message' => 'Erro ao listar os dados da marca',
+            'status' => Response::HTTP_NOT_FOUND,
+        ],
+        'updateBrand' => [
+            'message' => 'Erro ao atualizar os dados da marca',
+            'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+        ],
+        'deleteBrand' => [
+            'message' => 'Erro ao deletar os dados da marca',
+            'status' => Response::HTTP_NOT_FOUND,
+        ],
+    ];
 
     public function render($request)
     {
-        if ($this->getMessage() === 'getAllBrandsPaginated') {
-            $this->error = 'Erro ao listar os dados das marcas';
-            $this->statusCode = Response::HTTP_NOT_FOUND;
-        }
+        $errorMessage = $this->errorMessages[$this->getMessage()]['message'] ?? 'Erro desconhecido';
+        $statusCode = $this->errorMessages[$this->getMessage()]['status'] ?? Response::HTTP_INTERNAL_SERVER_ERROR;
 
-        if ($this->getMessage() === 'saveBrand') {
-            $this->error = 'Erro ao cadastrar os dados da marca';
-            $this->statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
-        }
-
-        if ($this->getMessage() === 'getBrandById') {
-            $this->error = 'Erro ao listar os dados da marca';
-            $this->statusCode = Response::HTTP_NOT_FOUND;
-        }
-
-        if ($this->getMessage() === 'updateBrand') {
-            $this->error = 'Erro ao atualizar os dados da marca';
-            $this->statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
-        }
-
-        if ($this->getMessage() === 'deleteBrand') {
-            $this->error = 'Erro ao deletar os dados da marca';
-            $this->statusCode = Response::HTTP_NOT_FOUND;
-        }
-
-        return response()->json(['success' => false,'error' => $this->getError()], $this->getStatusCode());
-    }
-
-    public function getError(): string
-    {
-        return $this->error;
-    }
-
-    public function getStatusCode(): int
-    {
-        return $this->statusCode;
+        return response()->json(['success' => false, 'error' => $errorMessage], $statusCode);
     }
 }
